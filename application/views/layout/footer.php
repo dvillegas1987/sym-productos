2d6ed509317f3118328caf30a5a881dc683de1d6
dvillegas1987 <dvillegas.soft@gmail.com>
2020-10-17 21:06:50 -0300

    Primer commit

---
+++ application/views/layout/footer.php
@@ -0,0 +1,38 @@
+      <!-- Footer -->
+      <footer class="footer pt-0">
+        <div class="row align-items-center justify-content-lg-between">
+          <div class="col-lg-6">
+            <div class="copyright text-center  text-lg-left  text-muted">
+              SISTEMA DE VENTAS<br/>
+              &copy; 2020 <a href="https://lightsthing.net" class="font-weight-bold ml-1" target="_blank">S&M Productos de Limpieza</a>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="copyright text-center  text-lg-left  text-muted">
+              ADMINISTRADORA <br/>
+              &copy; 2020 <a href="https://www.creative-tim.com/product/argon-dashboard" class="font-weight-bold ml-1" target="_blank">Sabrina Schaechtel</a>
+            </div>
+          </div>
+
+        </div>
+      </footer>
+    </div>
+  </div>
+  <!-- Argon Scripts -->
+  <!-- Core -->
+  <script src="<?php echo base_url(); ?>assets/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/jquery-scroll-lock/jquery-scrollLock.min.js"></script>
+  <!-- Optional JS -->
+  <script src="<?php echo base_url(); ?>assets/vendor/chart.js/Chart.min.js"></script>
+  <!-- Datatable -->
+  <script src="<?php echo base_url(); ?>assets/vendor/datatables.net/jquery.dataTables.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/datatables.net-responsive-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <!-- Sweet Alert 2 -->
+  <script src="<?php echo base_url(); ?>assets/vendor/sweetalert2/sweetalert2.min.js"></script>
+  <!-- Argon JS -->
+  <script src="<?php echo base_url(); ?>assets/js/argon.js?v=1.2.0"></script>
+</body>
+
+</html>
